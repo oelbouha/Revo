a09103640ddff1d0de7036e99d857658c806f26a
oelbouha <outmanelbouhali5@gmail.com>
2025-08-27 17:05:16 +0100

    adding under construction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ const linkStyle = "text-left text-xl sm:text-2xl md:text-5xl lg:text-6xl text-wh
 
   return (
     <header 
-      className="bg-transparent w-full z-50 absolute top-[3%] left-0 right-0" 
+      className="bg-transparent w-full z-50 absolute top-[3%] left-0 right-0"
       // style={{ position: 'absolute', top: 30, left: 0, right: 0 }}
     >
       <div className="w-full px-4 sm:px-10 md:px-12 ">
@@ -47,8 +47,8 @@ const linkStyle = "text-left text-xl sm:text-2xl md:text-5xl lg:text-6xl text-wh
           {/* Logo */}
           <div className="px-4">
             <img 
-              src={isBlack ? '/Logo-revo-black.png' : '/Group 7.svg'} 
-              alt="Revo Logo" 
+              src={isBlack ? '/Logo-revo-black.png' : '/Group 7.svg'}
+              alt="Revo Logo"
               className='w-20 sm:w-24 md:w-32 lg:w-56 xl:w-64 h-auto'
             />
           </div>
@@ -81,7 +81,7 @@ const linkStyle = "text-left text-xl sm:text-2xl md:text-5xl lg:text-6xl text-wh
               duration: 0.3,
               ease: 'easeInOut',
             }}
-            className="w-screen h-screen origin-top bg-black text-white pt-4 pb-10 pl-6 pr-4 md:pr-16 flex flex-col items-end justify-start"
+            className="w-screen h-screen origin-top bg-black text-white pt-4 pb-10 pl-6 pr-4 md:pr-16 flex flex-col items-end justify-start bg-cover"
             style={{ 
               position: 'fixed', 
               top: 0, 
@@ -91,12 +91,19 @@ const linkStyle = "text-left text-xl sm:text-2xl md:text-5xl lg:text-6xl text-wh
               backgroundSize: 'cover',
             }}
           >
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+              transition={{ duration: 0.8, ease: 'easeInOut' }}
+              className="w-56 h-56 absolute -top-16  -right-12 z-0 rounded-full bg-blue flex justify-end">
+            </motion.div>
             <button 
-              className="p-4 sm:p-8 transition-colors duration-300 px-4 sm:px-8 md:px-16"
+              className="p-4 z-10 sm:p-8 transition-colors duration-300 px-4 sm:px-8 md:px-16"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               {isMenuOpen && (
-                <div className="flex items-center space-x-3">
+                <div className="flex items-center space-x-3 ">
                   <span className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-medium">Close</span>
                   <div className='flex items-center justify-center z-99'>
                     <X className={`items-end w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 ${isMenuOpen ? 'text-white' : 'text-black'}`} />
