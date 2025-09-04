427944f5b033a7e56782c848227426e88515fba3
oelbouha <outmanelbouhali5@gmail.com>
2025-09-04 17:07:46 +0100

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
  
 import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
-import Index from "./pages/Index";
+
 import NotFound from "./pages/NotFound";
 import { P } from "node_modules/framer-motion/dist/types.d-Bq-Qm38R";
 import ProjectDetail from "./pages/projectDetail";
 import { Layout } from "./pages/Layout";
 import { Value } from "@radix-ui/react-select";
-
-import { Homepage } from "./pages/homepage/home";
+import {Homepage} from "./pages/homepage/Home";
 
 
 
