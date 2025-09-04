4da8b01df645c3bde0d19ee71812068940180cc0
oelbouha <outmanelbouhali5@gmail.com>
2025-09-04 11:26:42 +0100

    updating

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -114,10 +114,10 @@ const ImageSection = () => {
         <motion.img 
           src="https://static.vecteezy.com/system/resources/previews/039/063/358/large_2x/ai-generated-ai-circuit-board-technology-background-central-computer-processors-cpu-concept-motherboard-digital-chip-tech-science-background-integrated-communication-processor-3d-illustration-photo.jpeg" 
           alt="Studio workspace"
-          className="object-cover absolute top-0 left-0 h-[400px] md:h-[300px] lg:h-[444px]  xl:h-[550px] w-[50%]"
+          className="object-cover absolute top-0 left-0 h-[400px] md:h-[300px] lg:h-[444px]  xl:h-[550px] w-[49%]"
         />
        
-        <div className="flex justify-end items-center mt-20  h-[628px]    w-[73%]  bg-black text-white ">
+        <div className="flex justify-end items-center mt-20  h-[628px] w-[73%]  bg-black text-white ">
           <div className="max-w- mx-auto w-full flex justify-end bg-red- px-16 ml-[28%] lg:mr-[1%]   ">
             
             <h3 className="text-lg lg:text-3xl xl:text-3xl leading-relaxed font-Montserrat font-semibold px-8 text-right text-white">
