291f549355e503205b98625607ef2b86cf4df1ad
oelbouha <outmanelbouhali5@gmail.com>
2025-09-11 11:04:47 +0100

    version stable

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,18 +6,22 @@ import ScrollingTextBanner from './scrollingTextBanner';
 export const images = [
   {
     image: "/home1.png",
+    mobileImage: "/home11test.png",
     text: "Unlike anything else."
   },
   {
     image: "/home2.png",
+    mobileImage: "/home2test.png",
     text: "The future begins with a gesture"
   },
   {
-    image: "/home3.png",
+    image: "/home4.png",
+    mobileImage: "/home4test.png",
     text: "We reach for meaning"
   },
   {
-    image: "/home4.png",
+    image: "/home3.png",
+    mobileImage: "/home3test.png",
     text: "We don’t guess. We choose !"
   },
 ];
@@ -28,7 +32,7 @@ const ImageSlider = () => {
   useEffect(() => {
     const interval = setTimeout(() => {
       setSelectedImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
+    }, 6500);
 
     return () => clearTimeout(interval);
   }, [selectedImageIndex]);
@@ -36,20 +40,33 @@ const ImageSlider = () => {
   return (
     <div className="w-screen h-screen bg-black relative overflow-hidden">
       {/* Background Images */}
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="sync">
+        {/* Desktop Image */}
         <motion.img
-          key={selectedImageIndex}
+          key={`desktop-${selectedImageIndex}`}
           src={images[selectedImageIndex].image}
-          className="absolute w-full h-full object-cover top-0 left-0"
+          className="hidden md:block absolute w-full h-full object-cover top-0 left-0"
           initial={{ opacity: 0, scale: 1.1 }}
-          animate={{ opacity: 1 , scale: 1 }}
+          animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 1.5, ease: "easeInOut" }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        />
+
+        {/* Mobile Image */}
+        <motion.img
+          key={`mobile-${selectedImageIndex}`}
+          src={images[selectedImageIndex].mobileImage}
+          className="block md:hidden absolute w-full h-[100dvh] object-cover top-0 left-0"
+          initial={{ opacity: 0, scale: 1.1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
         />
       </AnimatePresence>
 
+
       {/* Text Overlay */}
-      <div className="absolute left-4 sm:left-2 md:left-4 top-[55%] transform -translate-y-1/2 z-10 max-w-[70%] p-4 sm:p-6 md:p-8">
+      <div className="absolute left-4 sm:left-2 md:left-4 top-[55%] transform -translate-y-1/2 z-10 max-w-[95%] p-4 sm:p-6 md:p-8">
         <div className="font-Montserrat font-bold overflow-hidden py-4">
           <AnimatePresence mode="wait">
             <motion.h1
@@ -57,8 +74,8 @@ const ImageSlider = () => {
               initial={{ opacity: 0, y: 0 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: 0 }}
-              transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
-              className="text-3xl md:text-5xl lg:text-7xl xl:text-8xl max-w-5xl"
+              transition={{ duration: 0.8, ease: "easeInOut", delay: 0 }}
+              className="text-[4rem] md:text-5xl lg:text-7xl xl:text-8xl max-w-7xl leading-tight text-white"
             >
               {images[selectedImageIndex].text}
             </motion.h1>
@@ -70,16 +87,11 @@ const ImageSlider = () => {
 };
 
 const HomeScreen = () => {
+
+
   return (
-    <div
-      className="relative h-[93dvh] md:min-h-screen flex items-center justify-center bg-gray-900 text-white overflow-hidden"
-    >
-      {/* Background Image Slider */}
+    <div className="relative h-screen md:min-h-screen flex items-center justify-center bg-gray-900 text-white overflow-hidden">
       <ImageSlider />
-
-      <Header />
-
-      {/* Scroll indicator */}
       <div className="absolute bottom-4 sm:bottom-6 md:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-4 h-6 sm:w-5 sm:h-8 md:w-6 md:h-10 border-2 border-white rounded-full flex justify-center">
           <div className="w-1 h-2 sm:h-3 bg-white rounded-full mt-2 animate-pulse"></div>
@@ -89,7 +101,10 @@ const HomeScreen = () => {
   );
 };
 
+
+
 const HeroSection = () => {
+
   return (
     <section
       id="home"
